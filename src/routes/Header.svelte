34f918bbc9704bd1f10ee063902a5ad011846cea
petethomasmusique <petethomasmusique@gmail.com>
2023-03-21 12:50:57 +0000

    develop: header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,38 +1,24 @@
 <script>
 	import { page } from '$app/stores';
-	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte'
     import logo from '$lib/images/q1synth-icon.svg';
 </script>
 
 <header>
     <nav class="container">
-        <img
-            src={logo}
-            class="mr-3 h-6 sm:h-6"
-            alt="Q1Synth Logo"
-        />
-        <ul class="menu">
-            <li class="menu__item"><a href="/about" >About</a></li>
-            <li class="menu__item"><a href="/help" >Help</a></li>
-            <li class="menu__item"><a href="/faq" >FAQ</a></li>
-        </ul>
-    
-    </nav>
-	<!-- <Navbar let:hidden let:toggle color="none" navClass="px-2 sm:px-4 py-3.5 w-full">
-        <NavBrand href="/">
+        <a href="/">
             <img
                 src={logo}
                 class="mr-3 h-6 sm:h-6"
                 alt="Q1Synth Logo"
             />
-            </NavBrand>
-            <NavHamburger on:click={toggle} />
-            <NavUl {hidden} ulClass="flex flex-col p-0 mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-                <NavLi href="/about" active={$page.url.pathname === '/about'} class="text-white uppercase text-sm tracking-widest">About</NavLi>
-                <NavLi href="/help" active={$page.url.pathname === '/help'} class="text-white uppercase text-sm tracking-widest">Help</NavLi>
-                <NavLi href="/faq" active={$page.url.pathname === '/faq'} class="text-white uppercase text-sm tracking-widest">FAQ</NavLi>
-            </NavUl>
-        </Navbar> -->
+        </a>
+        <ul class="menu">
+            <li class="menu__item"><a class={$page.url.pathname === '/about' ? 'active' : ''} href="/about">About</a></li>
+            <li class="menu__item"><a class={$page.url.pathname === '/help' ? 'active' : ''} href="/help">Help</a></li>
+            <li class="menu__item"><a class={$page.url.pathname === '/faq' ? 'active' : ''} href="/faq">FAQ</a></li>
+        </ul>
+    
+    </nav>
 </header>
 
 <style>
@@ -45,7 +31,8 @@
     nav {
         display: flex;
         justify-content: space-between;
-        padding: 1rem 0;
+        padding-top: 1rem;
+        padding-bottom: 1rem;
     }
 
     .menu {
@@ -64,12 +51,17 @@
 
     .menu__item a {
         text-transform: uppercase;
+        text-decoration: none;
         color: #FFF;
     }
 
     .menu__item a:hover {
-        color: var(--color-yellow);
+        color: var(--color-theme-2);
 
+    }
+
+    a.active {
+        color: var(--color-theme-2);
     }
 
     .menu__item:last-of-type {
