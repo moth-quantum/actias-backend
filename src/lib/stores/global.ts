4aa50b524495b9bddda1d03678decdb8f9bb9408
petethomasmusique <petethomasmusique@gmail.com>
2023-04-03 14:46:10 +0100

    volume control

--- src/lib/stores/global.ts
+++ src/lib/stores/global.ts
@@ -1,3 +1,8 @@
 import { writable } from 'svelte/store';
+import { output } from '../../sound';
 
 export const volume = writable(0.5);
+
+volume.subscribe((value) => {
+    output.gain.value = value;
+});
