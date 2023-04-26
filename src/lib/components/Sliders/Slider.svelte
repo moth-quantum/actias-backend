9294c78784569f823107940ea841980bbb2f0e8a
petethomasmusique <petethomasmusique@gmail.com>
2023-04-26 17:24:18 +0100

    slider-refactor: shakey but working

--- src/lib/components/Sliders/Slider.svelte
+++ src/lib/components/Sliders/Slider.svelte
@@ -6,6 +6,14 @@
     export let value: number = 0.5;
     export let colour: string = '#000';
     export let orientation: string = 'vertical';
+    
+    let isActive: boolean = false;
+    const handleMove = (e: MouseEvent) => {
+        if (isActive) {
+            const { height } = e.target?.getBoundingClientRect();
+            value = 1 - (height - e.offsetY) / height;
+        }
+    }
 </script>
 
 <div class="slider__container">
@@ -13,16 +21,16 @@
         class={'label label--' + orientation}
         style={`color: ${colour}`}    
     >{name}</span>
-    <!-- <input 
-        class={`${orientation}`} 
-        type="range" {min} {max} {step}
-        bind:value={value}
-        style={`background: ${colour}`}
-    /> -->
 
-    <div class="slider">
+
+    <div class="slider" 
+        on:mousedown={() => isActive = true}
+        on:mouseup={() => isActive = false}
+        on:mousemove={handleMove}
+    >
+        
         <div class="slider__track" style={`background: ${colour}`}></div>
-        <div class="slider__thumb"></div>
+        <div class="slider__thumb" style={`top: ${value * 100}%`}></div>
     </div>
 </div>
 
@@ -31,6 +39,8 @@
     .slider__container {
         display: flex;
         flex-direction: column;
+        align-items: center;
+        width: 1.25rem;
     }
     .slider {
         display: flex;
@@ -38,10 +48,23 @@
         position: relative;
         width: 100%;
         height: 100%;
+        width: 16px;
         &__track {
             width: 2px;
             height: 100%;
             background-color: blue;
+        }
+        &__thumb {
+            position: absolute;
+            top: 0;
+            right: 0;
+            left: 0;
+            width: 100%;
+            height: 8px;
+            background: linear-gradient(to bottom, #000, #000 35%, var(--color-grey-light) 35%, var(--color-grey-light) 65%, #000 65%);
+            border-radius: 0;
+            cursor: pointer;
+            box-shadow: 2px 3px 5px 1px var(--color-box-shadow);
         }
     }
 
@@ -49,42 +72,11 @@
         color: var(--color-grey-light);
         font-size: var(--text-base);
         text-align: center;
-        margin-right: 1rem;
         margin-bottom: 0.5rem;
-        min-width: 0.5rem;
         width: 100%;
     }
 
-    input[type=range] {
-        appearance: none;
-        outline: none; 
-        margin: 0;
-        width: calc(100% - 1rem);
-        height: 2px;
-    }
-    
-    /* Annoyingly, lots of repeated css, but doesn't work otherwise */
-    input::-webkit-slider-thumb {
-        -webkit-appearance: none;
-        appearance: none;
-        width: 8px;
-        height: 16px;
-        background: linear-gradient(to left, #000, #000 35%, var(--color-grey-light) 35%, var(--color-grey-light) 65%, #000 65%);
-        border-radius: 0;
-        cursor: pointer;
-        position: relative;
-        box-shadow: 2px 3px 5px 1px var(--color-box-shadow);
-    }
-
-    input:-moz-range-thumb {
-        -webkit-appearance: none;
-        appearance: none;
-        width: 8px;
-        height: 16px;
-        background: linear-gradient(to left, #000, #000 35%, var(--color-grey-light) 35%, var(--color-grey-light) 65%, #000 65%);
-        border-radius: 0;
-        cursor: pointer;
-        position: relative;
-        box-shadow: 2px 3px 5px 1px var(--color-box-shadow);
+    input {
+        display: none;
     }
 </style>
