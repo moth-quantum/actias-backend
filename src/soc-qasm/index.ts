a49997bf249c175490963187acd4d16fa6e2a1c2
petethomasmusique <petethomasmusique@gmail.com>
2023-05-02 21:06:01 +0100

    socket init. No connection

--- src/soc-qasm/index.ts
+++ src/soc-qasm/index.ts
@@ -4,10 +4,11 @@ export const socket = io("http://soc-qasm.cephasteom.co.uk");
 
 console.log(socket)
 
-socket.on("connect", () => {
-    console.log("connected to server",socket.id);
-});
-socket.on("disconnect", () => {
-    console.log("server disconnected",socket.id);
-});
+socket.on('connect', () => console.log('connected'))
+socket.on('disconnect', () => console.log('disconnected'))
+socket.on('response', data => data[0] === 'info' && console.log(data[1]));
 
+export function sendQasm(x: number, y: number, z: number, backend: string, password: string) {
+    const qasm = `OPENQASM 2.0;\ninclude "qelib1.inc";\nqreg q[1];\ncreg c[1];\nu(${(x * Math.PI)/2},${(y * Math.PI)/2},${(z * Math.PI)/2}) q[0];\nmeasure q[0] -> c[0];\n`
+    socket.emit('QuTune', [password, qasm, 1, backend])
+}
