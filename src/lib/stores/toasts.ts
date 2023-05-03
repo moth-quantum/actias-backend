763361ec279cee501cd1b658cc5b321012ec7e6d
petethomasmusique <petethomasmusique@gmail.com>
2023-05-03 20:50:38 +0100

    welcome message

--- src/lib/stores/toasts.ts
+++ src/lib/stores/toasts.ts
@@ -1,10 +1,10 @@
 import { writable, get } from 'svelte/store';
 
 export const toasts = writable<{message: string, type: string}[]>([
-    // {message: 'Welcome to QuTune!', type: 'info'},
+    {message: 'Welcome to the Q1 Synth. Please use Chrome for optimum performance.', type: 'info'},
 ]);
 
-toasts.subscribe(() => setTimeout(() => toasts.set(get(toasts).slice(1)), 3000))
+toasts.subscribe(() => setTimeout(() => toasts.set(get(toasts).slice(1)), 5000))
 
 export function addToast(message: string, type: string = 'info') {
     toasts.update(toasts => [...toasts, {message, type}]);
