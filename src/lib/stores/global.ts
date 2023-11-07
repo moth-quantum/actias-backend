f3ef5c0cf5a1ba544e447ee45225be7333258315
petethomasmusique <petethomasmusique@gmail.com>
2023-11-07 15:21:19 +0000

    electron: put isApp conditional in store

--- src/lib/stores/global.ts
+++ src/lib/stores/global.ts
@@ -1,7 +1,10 @@
-import { writable } from 'svelte/store';
+import { writable, get } from 'svelte/store';
 
 export const volume = writable(0.5);
 
 export const mute = writable(false);
 
 export const fullscreen = writable(false);
+
+// @ts-ignore
+export const isApp = writable(!!window.isApp); // used for conditional functionality between web and app
