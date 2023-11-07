aef3d8546425efa9cd06a68c61ebfb536ac7fe36
petethomasmusique <petethomasmusique@gmail.com>
2023-11-07 11:31:05 +0000

    electron: replace svelte default adaptor with custom svelte-electron adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/kit/vite";
+// import adapter from "@sveltejs/adapter-auto"; // original sveltejs adapter
+import adapter from "@ptkdev/sveltekit-electron-adapter";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -17,7 +18,7 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: adapter({strict: false}),
   },
 };
 
