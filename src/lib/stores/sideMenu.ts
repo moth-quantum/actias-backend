bb4c2487425a1c155b5d448fdc2914c12d9a6730
petethomasmusique <petethomasmusique@gmail.com>
2024-03-13 11:49:16 +0000

    epic/phase-2: update midi icon

--- src/lib/stores/sideMenu.ts
+++ src/lib/stores/sideMenu.ts
@@ -2,14 +2,15 @@ import { writable, derived } from 'svelte/store';
 import { isApp } from '$lib/stores/global';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import keyboard from '$lib/images/keyboard.svg';
-import { faGlobe, faUser, faUsers, faCircle, faCircleQuestion, faGear, faPlay } from '@fortawesome/free-solid-svg-icons';
-library.add(faGlobe, faUser, faUsers, faCircle, faCircleQuestion, faGear, faPlay);
+import midi from '$lib/images/midi-white.svg';
+import { faGlobe, faUser, faUsers, faCircleQuestion, faGear, faPlay } from '@fortawesome/free-solid-svg-icons';
+library.add(faGlobe, faUser, faUsers, faCircleQuestion, faGear, faPlay);
 
 export const menuItems = writable([
-    { name: 'profile', icon: faUser, isVisible: true, isActive: false, hasSubMenu: true },
-    { name: 'assign qubits', icon: faGlobe, isVisible: true, isActive: false, hasSubMenu: true },
+    { name: 'profile', icon: faUser, isVisible: isApp(), isActive: false, hasSubMenu: true },
+    { name: 'assign qubits', icon: faGlobe, isVisible: isApp(), isActive: false, hasSubMenu: true },
     { name: 'connect', icon: faUsers, isVisible: isApp(), isActive: false, hasSubMenu: true },
-    { name: 'midi', icon: faCircle, isVisible: true, isActive: false, hasSubMenu: true },
+    { name: 'midi', image: midi, isVisible: true, isActive: false, hasSubMenu: true },
     { name: 'keyboard', image: keyboard, isVisible: true, isActive: false, hasSubMenu: false },
     { name: 'tooltips', icon: faCircleQuestion, isVisible: true, isActive: false, hasSubMenu: false },
     { name: 'perform', icon: faPlay, isVisible: true, isActive: false, hasSubMenu: false }
