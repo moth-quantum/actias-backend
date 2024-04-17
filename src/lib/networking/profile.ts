29881bfbb963e69ac83c0427123c49dbf26bef6d
petethomasmusique <petethomasmusique@gmail.com>
2024-04-17 12:09:45 +0100

    feature/users: listen out for name and location changes and update api

--- src/lib/networking/profile.ts
+++ src/lib/networking/profile.ts
@@ -11,36 +11,30 @@ export const updateProfile = () => {
         'Content-Type': 'application/json',
     }
     
+    // listen for changes to the name and location stores
     const unsubscribeName = name.subscribe(debounce((value: string) => {
-        fetch(endpoint, {
-            method,
-            headers,
-            body: JSON.stringify({
-                "name": value,
-            })
-        })
-        .then(response => response.json())
-        .then((data) => {
-            console.log('updated name', data)
-            localStorage.setItem('q.name', value);
-        })
+        // update local storage value so that it persists
+        localStorage.setItem('q.name', value);
+        
+        // update the api
+        const body = JSON.stringify({"name": value});
+        fetch(endpoint, { method, headers, body })
+            .catch((error) => console.error('Error:', error));
+
     }, 500))
 
     const unsubscribeLocation = location.subscribe(debounce((value: string) => {
-        fetch(endpoint, {
-            method,
-            headers,
-            body: JSON.stringify({
-                "location": value,
-            })
-        })
-        .then(response => response.json())
-        .then((data) => {
-            console.log('updated location', data)
-            localStorage.setItem('q.location', value);
-        })
+        // update local storage value so that it persists
+        localStorage.setItem('q.location', value);
+        
+        // update the api
+        const body = JSON.stringify({"location": value});
+        fetch(endpoint, { method, headers, body })
+            .catch((error) => console.error('Error:', error));
+
     }, 500))
 
+    // return a function to unsubscribe from the stores
     return () => {
         unsubscribeName();
         unsubscribeLocation();
