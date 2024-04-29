7b6924c4d5782d8ae9760be1a75f21a828ac23da
petethomasmusique <petethomasmusique@gmail.com>
2024-04-29 13:24:44 +0100

    epic/phase-2: remove hardcoded credentials

--- src/lib/networking/config.ts
+++ src/lib/networking/config.ts
@@ -9,14 +9,3 @@ export const headers = derived([apiToken], ([$apiToken]) => {
         'Content-Type': 'application/json',
     }
 })
-
-// TODO: API domain from Electron APP
-// export const apiDomain = 'http://moth-q1-api.test';
-// // TODO: bearer token from Electron APP
-// export const apiToken = '1|lu2SHkmUrKAQIl8OwKMPo7ReZkpu5lsh47rYWmF74a223638';
-// export const headers = {
-//     'Authorization' : `Bearer ${apiToken}`,
-//     'Content-Type': 'application/json',
-// };
-// TODO: get key and host from the Electron app
-// export const pusherKey = 'a2eopc5dh3ifhlbhddcf';
