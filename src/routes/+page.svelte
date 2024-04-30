794b772e6e3bcc1d8a87391b2381f0017a4881f2
Charlotte <ckdrew670@hotmail.com>
2024-04-30 13:57:13 +0100

    Hook up the sidebar button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
     import { onMount } from 'svelte';
     import { startAudio } from '../sound';
     import { fullscreen as fs, showKeyboard, showSideMenu, isApp } from '$lib/stores/global';
+    import { menuItems } from '$lib/stores/sideMenu';
     import { redrawCables } from '$lib/stores/patching';
     import { activeQubitCount } from '$lib/stores/qubits';
     import Presets from '$lib/components/Presets/Presets.svelte';
@@ -31,6 +32,7 @@
     
     library.add(faBars);
 
+    let performanceModeActive = false;
     let isDesktop = false;
     let sidebarIsHidden = true; 
     let transitionParams = {
@@ -48,7 +50,13 @@
         isDesktop = window.innerWidth > 1200
         redrawCables(500)
         
-        if(!isApp()) return
+        const unsubscribeMenuItems = menuItems.subscribe(items => {
+            let performanceMode = items.find(item => item.name === 'perform');
+            performanceModeActive = performanceMode?.isActive || false;
+            console.log(performanceModeActive)
+        });
+
+        if(!isApp()) return () => unsubscribeMenuItems()
         
         initElectronAPI()
         
